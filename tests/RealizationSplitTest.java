bd46203412a75f2bb3bffb62c29642dba9b8443a
Антон Натура <p222pp22@yandex.ru>
2018-04-18 18:42:43 +0300

    rewrite

--- tests/RealizationSplitTest.java
+++ tests/RealizationSplitTest.java
@@ -1,81 +1,82 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RealizationSplitTest {
+
+    private static String content(String file) throws IOException {
+        Scanner fileTemp = new Scanner(new FileReader(file));
+        StringBuilder result = new StringBuilder("");
+        while (fileTemp.hasNext()){
+            result.append(fileTemp.nextLine());
+            if (fileTemp.hasNext())
+                result.append("\n");
+        }
+        fileTemp.close();
+        return result.toString();
+    }
     @Test
     void funL() throws IOException {
-        String[] arg = {"-d", "-l 10", "-o", "input.txt"};
+        String[] arg = {"-d", "-l 10", "-o yes", "input/input.txt"};
         Split.main(arg);
-        String[] arg1 = {"-d", "-l 10", "-o yes", "input.txt"};
-        Split.main(arg1);
-        String[] arg2 = {"-d", "-l 10", "-o -", "input.txt"};
-        Split.main(arg2);
-        String[] arg3 = {"-d", "-l 10", "input.txt"};
-        Split.main(arg3);
+        assertEquals(content("yes1.txt"), content("funLoutputs/l1.txt"));
+        assertEquals(content("yes2.txt"), content("funLoutputs/l2.txt"));
+        assertEquals(content("yes3.txt"), content("futLoutputs/l3.txt"));
+        assertEquals(content("yes4.txt"), content("funLoutputs/l4.txt"));
     }
 
     @Test
     void funC() throws IOException {
-        String[] arg = {"-d", "-c 200", "-o", "input.txt"};
+        String[] arg = {"-d", "-c 350", "-o yes", "input/input.txt"};
         Split.main(arg);
-        String[] arg1 = {"-d", "-c 200", "-o yes", "input.txt"};
-        Split.main(arg1);
-        String[] arg2 = {"-d", "-c 200", "-o -", "input.txt"};
-        Split.main(arg2);
-        String[] arg3 = {"-d", "-c 200", "input.txt"};
-        Split.main(arg3);
+        assertEquals(content("yes1.txt"), content("funLoutputs/c1.txt"));
+        assertEquals(content("yes2.txt"), content("funLoutputs/c2.txt"));
+        assertEquals(content("yes3.txt"), content("futLoutputs/c3.txt"));
+        assertEquals(content("yes4.txt"), content("funLoutputs/c4.txt"));
     }
 
     @Test
     void funN() throws IOException {
-        String[] arg = {"-d", "-n 5", "-o", "input.txt"};
+        String[] arg = {"-d", "-n 4", "-o yes", "input/input.txt"};
         Split.main(arg);
-        String[] arg1 = {"-d", "-n 5", "-o yes", "input.txt"};
-        Split.main(arg1);
-        String[] arg2 = {"-d", "-n 5", "-o -", "input.txt"};
-        Split.main(arg2);
-        String[] arg3 = {"-d", "-n 5", "input.txt"};
-        Split.main(arg3);
+        assertEquals(content("yes1.txt"), content("funLoutputs/l1.txt"));
+        assertEquals(content("yes2.txt"), content("funLoutputs/l2.txt"));
+        assertEquals(content("yes3.txt"), content("futLoutputs/l3.txt"));
+        assertEquals(content("yes4.txt"), content("funLoutputs/l4.txt"));
     }
 
     @Test
     void renameL() throws IOException {
-        String[] arg = {"-l 10", "-o", "input.txt"};
+        String[] arg = {"-l 10", "-o yes", "input/input.txt"};
         Split.main(arg);
-        String[] arg1 = {"-l 10", "-o yes", "input.txt"};
-        Split.main(arg1);
-        String[] arg2 = {"-l 10", "-o -", "input.txt"};
-        Split.main(arg2);
-        String[] arg3 = {"-l 10", "input.txt"};
-        Split.main(arg3);
+        assertEquals(content("yesaa.txt"), content("funLoutputs/l1.txt"));
+        assertEquals(content("yesab.txt"), content("funLoutputs/l2.txt"));
+        assertEquals(content("yesac.txt"), content("futLoutputs/l3.txt"));
+        assertEquals(content("yesad.txt"), content("funLoutputs/l4.txt"));
     }
 
     @Test
     void renameC() throws IOException {
-        String[] arg = {"-c 250", "-o", "input.txt"};
+        String[] arg = {"-c 350", "-o yes", "input/input.txt"};
         Split.main(arg);
-        String[] arg1 = {"-c 250", "-o yes", "input.txt"};
-        Split.main(arg1);
-        String[] arg2 = {"-c 250", "-o -", "input.txt"};
-        Split.main(arg2);
-        String[] arg3 = {"-c 250", "input.txt"};
-        Split.main(arg3);
+        assertEquals(content("yesaa.txt"), content("funLoutputs/c1.txt"));
+        assertEquals(content("yesab.txt"), content("funLoutputs/c2.txt"));
+        assertEquals(content("yesac.txt"), content("futLoutputs/c3.txt"));
+        assertEquals(content("yesad.txt"), content("funLoutputs/c4.txt"));
     }
 
     @Test
     void renameN() throws IOException {
-        String[] arg = {"-n 3", "-o", "input.txt"};
+        String[] arg = {"-l 4", "-o yes", "input/input.txt"};
         Split.main(arg);
-        String[] arg1 = {"-n 3", "-o yes", "input.txt"};
-        Split.main(arg1);
-        String[] arg2 = {"-n 6", "-o -", "input.txt"};
-        Split.main(arg2);
-        String[] arg3 = {"-n 7", "input.txt"};
-        Split.main(arg3);
-        //assertEquals();
+        assertEquals(content("yesaa.txt"), content("funLoutputs/l1.txt"));
+        assertEquals(content("yesab.txt"), content("funLoutputs/l2.txt"));
+        assertEquals(content("yesac.txt"), content("futLoutputs/l3.txt"));
+        assertEquals(content("yesad.txt"), content("funLoutputs/l4.txt"));
     }
 
 }
